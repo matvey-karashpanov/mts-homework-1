28ae7d12343b27d6bdb95ca723a7215b26e241e4
Ailana <karamchakova.ae@phystech.edu>
2026-03-23 03:17:01 +0300

    implement repository layer with InMemory and Stub implementations

--- src/main/java/com.mipt.ailanakaramchakova/repository/StubTaskRepository.java
+++ src/main/java/com.mipt.ailanakaramchakova/repository/StubTaskRepository.java
@@ -1,26 +1,30 @@
 package com.mipt.ailanakaramchakova.repository;
 
 import com.mipt.ailanakaramchakova.model.Task;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 /**
  * Stub implementation of TaskRepository with fixed data.
- * Created manually via @Bean in AppConfig.
  */
 public class StubTaskRepository implements TaskRepository {
 
   @Override
   public List<Task> findAll() {
     List<Task> tasks = new ArrayList<>();
-    tasks.add(new Task(1L, "Stub Task", "Description", false));
+    Task task = new Task(1L, "Stub Task", "Description", false, LocalDateTime.now(), null, null,
+      null);
+    tasks.add(task);
     return tasks;
   }
 
   @Override
   public Optional<Task> findById(Long id) {
-    return Optional.of(new Task(1L, "Stub Task", "Description", false));
+    Task task = new Task(1L, "Stub Task", "Description", false, LocalDateTime.now(), null, null,
+      null);
+    return Optional.of(task);
   }
 
   @Override
