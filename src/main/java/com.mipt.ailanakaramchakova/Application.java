9128062c0d6afc150aeb01bbddcb8c278fa26feb
Ailana <karamchakova.ae@phystech.edu>
2026-04-08 22:34:33 +0300

    add @EnableJpaAuditing for automatic audit fields

--- src/main/java/com.mipt.ailanakaramchakova/Application.java
+++ src/main/java/com.mipt.ailanakaramchakova/Application.java
@@ -3,13 +3,14 @@ package com.mipt.ailanakaramchakova;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
- * Main application class.
- * Starts the Spring Boot application and enables AspectJ auto proxying.
+ * Main application class. Starts the Spring Boot application and enables AspectJ auto proxying.
  */
 @SpringBootApplication
 @EnableAspectJAutoProxy
+@EnableJpaAuditing
 public class Application {
 
   public static void main(String[] args) {
